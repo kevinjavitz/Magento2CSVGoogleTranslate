c081ed047049ea0c1610b9f0fd6f4994f439752f
kjavitz <kjavitz@itwebexperts.com>
2020-11-05 11:24:21 +0545

    updated with latest google cloud apis env.php file

---
+++ env.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Set your developer API key here, make sure it is enabled for the domain you are using this from
+ * and Google Translate API https://console.developers.google.com
+**/
+$devkey = '';
