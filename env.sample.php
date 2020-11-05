252950330bfb9ea6509874093e5d68fe972ddcc2
kjavitz <kjavitz@itwebexperts.com>
2020-11-05 11:30:03 +0545

    env.php file renamed to env.sample.php

--- env.sample.php
+++ env.sample.php
@@ -2,5 +2,6 @@
 /**
  * Set your developer API key here, make sure it is enabled for the domain you are using this from
  * and Google Translate API https://console.developers.google.com
+ * Rename this file to env.php
 **/
 $devkey = '';
